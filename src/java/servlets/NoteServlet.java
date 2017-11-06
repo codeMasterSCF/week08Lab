95afb54772ae8db434f515c5ea895706057d9afc
463849 <463849@ELF6IT215756.ACDM.DS.SAIT.CA>
2017-11-06 12:17:41 -0700

    Finished App

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -3,7 +3,6 @@ package servlets;
 import businesslogic.NoteService;
 import domainmodel.Note;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
